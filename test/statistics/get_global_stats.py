636332b9af65ca3f993064688c820e99289846dd
Serafim Nenarokov <serafim221@gmail.com>
2018-12-07 17:31:21 +0100

    Change global statistics: restriction by types

--- test/statistics/get_global_stats.py
+++ test/statistics/get_global_stats.py
@@ -11,9 +11,8 @@ description = 'Get global statistics of contaminations for the Winston output'
 parser = argparse.ArgumentParser(description=description)
 parser.add_argument("-r", "--results_path", required=True, help="Winston results folder")
 parser.add_argument("-o", "--output", required=True, help="Full statistics output file (.csv)")
-parser.add_argument("-t", "--types_path", required=True, help="Path to types file (.csv)")
+parser.add_argument("-t", "--types", required=False, help="Comma separated types for calculation, all if not provided")
 options = parser.parse_args()
-
 
 def get_contigs_count(path):
     result = 0
@@ -27,38 +26,43 @@ def get_contigs_count(path):
     return result
 
 
-def parse_types(path):
-    result = {}
-    with open(path) as f:
-        for line in f.readlines():
-            splitted = line.strip().split(',')
-            result[(splitted[0], splitted[1])] = splitted[-1]
-    return result
-
-
 def main():
-    types = parse_types(options.types_path)
-    paths = glob.glob(ntpath.join(options.results_path, "*_contaminations.csv"))
+    paths = glob.glob(ntpath.join(options.results_path, "*_deleted_stats.csv"))
     paths.sort()
     acc_regexp = re.compile("MMETSP\d{4}")
 
+    types = []
+    if options.types:
+        types = options.types.split(',')
+
+    without_type_restriction = len(types) == 0
+
     with open(options.output, 'w') as out_f:
-        out_f.write("to_id,from_id,contam_cnt,contig_cnt,clean_cnt,deleted_cnt,type\n")
+        out_f.write("acc_id,contig_cnt,deleted_cnt\n")
 
         for path in tqdm(paths):
             acc_id = acc_regexp.search(ntpath.basename(path))[0]
+            deleted_cnt = 0
+
+            with open(path) as f:
+                last_contig = None
+
+                for line in f.readlines():
+                    data = line.strip().split(',')
+                    contig = data[0]
+
+                    if without_type_restriction or data[-2] in types:
+                        if contig != last_contig:
+                            deleted_cnt += 1
+                            last_contig = contig
 
             clean_path = ntpath.join(options.results_path, f"{acc_id}_clean.fasta")
             deleted_path = ntpath.join(options.results_path, f"{acc_id}_deleted.fasta")
 
-            clean_cnt = get_contigs_count(clean_path)
-            deleted_cnt = get_contigs_count(deleted_path)
+            all_clean_cnt = get_contigs_count(clean_path)
+            all_deleted_cnt = get_contigs_count(deleted_path)
 
-            with open(path) as f:
-                for line in f.readlines():
-                    right_acc_id = line.strip().split(',')[0]
-                    out_f.write(
-                        f"{acc_id},{line.strip()},{clean_cnt + deleted_cnt},{clean_cnt},{deleted_cnt},{types[(acc_id, right_acc_id)]}\n")
+            out_f.write(f"{acc_id},{all_clean_cnt+all_deleted_cnt},{deleted_cnt}\n")
 
 
 if __name__ == '__main__':
